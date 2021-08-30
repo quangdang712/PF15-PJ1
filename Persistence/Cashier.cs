801a24c25c74f1915e73f2cc2a7aa26d60e0c0e3
quangdang712 <57580223+quangdang712@users.noreply.github.com>
2021-08-30 10:35:14 +0700

    Add files via upload

---
+++ Persistence/Cashier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Persistence
+{
+    public class Cashier
+    {
+        public int CashierId {set;get;}
+        public string CashierName {set;get;}
+        public string UserName {set;get;}
+        public string Password {set;get;}
+        public string Telephone {set;get;}
+
+        
+    }
+}
