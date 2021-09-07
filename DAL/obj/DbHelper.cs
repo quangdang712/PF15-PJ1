ec824e18a8f85f541fba70dbd06d5a10f9cb2df6
quangdang712 <57580223+quangdang712@users.noreply.github.com>
2021-09-08 00:24:34 +0700

    Add files via upload

---
+++ DAL/obj/DbHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MySql;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    public class DbHelper
+    {
+        private static MySqlConnection connection;
+        public static MySqlConnection GetConnection()
+        {
+            if (connection == null)
+            {
+                connection = new MySqlConnection
+                {
+                    ConnectionString = "server=localhost;user id=vtca;password=vtcacademy;port=3306;database=PJ1"
+                };
+            }
+            return connection;
+        }
+        private DbHelper(){}
+    }
+}
